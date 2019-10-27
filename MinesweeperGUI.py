578717da342a7b428f4fc7708ff6dc098b866e4b
Kavin Adithiya <kavin.avs@gmail.com>
2019-10-27 15:22:04 -0400

    Added Display for Testing
    Added pygame display to test GUI functions and formating
    This will handle clicks on the gui and updates board
    accordingly.

--- MinesweeperGUI.py
+++ MinesweeperGUI.py
@@ -1,2 +1,32 @@
 import pygame
+import TileView
+
+pygame.init()
+clock = pygame.time.Clock()
+
+crashed = False
+menu_height = 50
+board_height = 600
+board_width = 600
+width = board_width
+height = menu_height + board_height
+
+screen = pygame.display.set_mode((height, width))
+screen.fill((200, 200, 200))
+pygame.display.set_caption('MineSweeperV2.0')
+
+# Initial board creation, using tiles from board and Button class
+
+
+while not crashed:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(event.pos)
+
+
+    pygame.display.update()
 
